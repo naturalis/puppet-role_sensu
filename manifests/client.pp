fcbb6800e4c8f56ef9cc7cd553d44bc6b949c9f3
Atze de Vries <atze.devries@naturalis.nl>
2014-10-24 10:12:52 +0200

    added some files

---
+++ manifests/client.pp
@@ -0,0 +1,35 @@
+#
+#
+#
+class role_sensu::client(
+  $sensu_server       = 'changeme',
+  $rabbitmq_password  = 'changeme',
+  $sensu_cluster_name = 'changeme',
+  )
+{
+  if $sensu_server == 'changeme' { fail ('change the var $sensu_server to something else (and not changeme)')}
+  if $rabbitmq_password == 'changeme' { fail('please change the rabbitmq_password') }
+  if $sensu_cluster_name == 'changeme' { fail('please change the sensu_cluser_name') }
+
+
+  class { 'sensu':
+    rabbitmq_password  => $rabbitmq_password,
+    rabbitmq_host      => $sensu_server,
+    subscriptions      => 'sensu-test',
+  }
+
+  sensu::check { 'check_cron':
+    command     => '/bin/ps -aux | grep -v grep | grep cron',
+    handlers    => 'default',
+    subscribers => 'sensu-test'
+  }
+
+  @@sensu::check { "check_ping_of_${::fqdn}":
+    command     => 'echo 0',
+    handlers    => 'default',
+    subscribers => 'sensu-test'
+    standalone  => true,
+    tag         => "sensu_check_${sensu_cluster_name}",
+  }
+
+}
