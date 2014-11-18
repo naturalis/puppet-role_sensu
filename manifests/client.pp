f58527fd7d3b47862f4bc0a5228e3a65877437d1
Atze de Vries <atze.devries@naturalis.nl>
2014-11-18 13:58:10 +0100

    adding catalogue of life check

--- manifests/client.pp
+++ manifests/client.pp
@@ -35,10 +35,10 @@ class role_sensu::client(
 
   # this is not a registrated check
   @@sensu::check { 'check_www_catalogue_of_life_org':
-    command     => '/opt/sensu-community-plugins/plugins/http/check-http.rb --url http://www.catalogueoflife.org -q \'Welcome to the Catalogue of Life website\'',
+    command     => '/opt/sensu-community-plugins/plugins/http/check-http.rb --url http://www.catalogueoflife.org -q \'Welcome to the Catalogue of Life websitesadfsd\'',
     handlers    => 'default',
     subscribers => 'sensu-test',
-    standalone  => true,
+    standalone  => false,
     tag         => "sensu_check_${sensu_cluster_name}",
   }
 
