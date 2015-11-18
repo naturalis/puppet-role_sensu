1e9028a64856012f2213b1a65120f00e50cfcc28
Atze de Vries <atze.devries@naturalis.nl>
2015-11-18 10:34:47 +0100

    aaaa

--- manifests/client.pp
+++ manifests/client.pp
@@ -21,7 +21,7 @@ class role_sensu::client(
     rabbitmq_host            => $sensu_server,
     subscriptions            => 'sensu-test',
     use_embedded_ruby        => true,
-    client_port              => {'port' => '3030'},
+    client_custom              => {'port' => '3030'},
     rabbitmq_port            => 5671,
     rabbitmq_vhost           => '/sensu',
     client_keepalive         => { 'handlers' => ['default'] }
