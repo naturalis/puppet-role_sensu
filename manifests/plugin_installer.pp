17fe23b0a0143cc79d34cc52a79edf1a12d3f542
Atze de Vries <atze.devries@naturalis.nl>
2015-11-18 15:27:46 +0100

    adf

--- manifests/plugin_installer.pp
+++ manifests/plugin_installer.pp
@@ -5,12 +5,17 @@ define role_sensu::plugin_installer (
   $version = undef,
 ) {
 
-  package { $title:
-    ensure          => $version,
-    provider        => gem,
-    install_options => [{
-      '--install-dir' => '/opt/sensu/embedded/lib/ruby/gems/2.0.0'
-    }],
+  exec { "Installing Sensu Gem: ${title}" :
+    cmd    => "/opt/sensu/embedded/bin/gem --install-dir /opt/sensu/embedded/lib/ruby/gems/2.0.0 ${title}",
+    unless => "/opt/sensu/embedded/bin/gem list --local | grep ${title}"
   }
+
+  # package { $title:
+  #   ensure          => $version,
+  #   provider        => gem,
+  #   install_options => [{
+  #     '--install-dir' => '/opt/sensu/embedded/lib/ruby/gems/2.0.0'
+  #   }],
+  # }
 
 }
