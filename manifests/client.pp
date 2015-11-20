92c4e53f45f4bda78c53551d0eccc680237ffb6c
Atze de Vries <atze.devries@naturalis.nl>
2015-11-20 21:58:33 +0100

    tipo

--- manifests/client.pp
+++ manifests/client.pp
@@ -49,7 +49,7 @@ class role_sensu::client(
   }
 
   if $reboot_warning {
-    $builtin_plugins['check_for_reboot_required'] = {'command' => 'if [ -f /var/run/reboot-required ]; then return 1; fi' }
+    $builtin_checks['check_for_reboot_required'] = {'command' => 'if [ -f /var/run/reboot-required ]; then return 1; fi' }
   }
 
   if $check_disk {
