79a03e3be5a98de063394a4620babdb4e61f30ad
Atze de Vries <atze.devries@naturalis.nl>
2015-11-24 10:52:19 +0100

    stuppppppid

--- manifests/check_process_installer.pp
+++ manifests/check_process_installer.pp
@@ -7,7 +7,7 @@ define role_sensu::check_process_installer(
 
   $ruby_run_comand = '/opt/sensu/embedded/bin/ruby -C/opt/sensu/embedded/bin'
   $hash = { "check_process_${title}" =>
-    {'command' => "${ruby_run_comand} check-process.rb -p -${title}"}
+    {'command' => "${ruby_run_comand} check-process.rb -p ${title}"}
   }
   create_resources( 'sensu::check', $hash, $checks_defaults )
 }
