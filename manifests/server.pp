dd4c57c750110f5df889ef9a00ba8518f9f74384
AtzeDeVries <atze.devries@naturalis.nl>
2017-06-13 13:41:28 +0200

    nginx change

--- manifests/server.pp
+++ manifests/server.pp
@@ -140,7 +140,7 @@ class role_sensu::server(
     members => ['localhost:3000'],
   }
 
-  nginx::resource::vhost { $uchiwa_dns_name :
+  nginx::resource::server { $uchiwa_dns_name :
     proxy       => 'http://sensu_naturalis_nl',
     ssl         => true,
     listen_port => 443,
@@ -154,7 +154,7 @@ class role_sensu::server(
       members => ['localhost:4567'],
     }
 
-    nginx::resource::vhost { 'sensu api expose' :
+    nginx::resource::server { 'sensu api expose' :
       proxy       => 'http://sensuapi_naturalis_nl',
       ssl         => true,
       listen_port => 8443,
