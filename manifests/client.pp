b1e0794bbd6193d87950c5ea3ee681ba7583ed38
AtzeDeVries <atze.devries@naturalis.nl>
2017-10-05 09:53:59 +0200

    hopefully defaults fix

--- manifests/client.pp
+++ manifests/client.pp
@@ -23,7 +23,7 @@ class role_sensu::client(
     interval      => 600,
     occurrences   => 3,
     refresh       => 60,
-    handlers      => [ 'default'],
+    handlers      => ['default'],
     subscribers   => ['appserver'],
     standalone    => true },
 
@@ -104,7 +104,7 @@ class role_sensu::client(
     defaults => $checks_defaults,
   }
 
-  Sensu::Check <| tag == 'central_sensu' |> {
+  Sensu::Check {
     interval    => $checks_defaults['interval'],
     occurrences => $checks_defaults['occurrrences'],
     refresh     => $checks_defaults['refresh'],
@@ -112,6 +112,8 @@ class role_sensu::client(
     subscribers => $checks_defaults['subscribers'],
     standalone  => $checks_defaults['standalone'],
   }
+
+  Sensu::Check <| tag == 'central_sensu' |>
 
   create_resources( 'sensu::handler' , $handler_definitions, {} )
   #create_resources('role_sensu::plugin_installer', unique(concat($plugins, $builtin_plugins)), [])
